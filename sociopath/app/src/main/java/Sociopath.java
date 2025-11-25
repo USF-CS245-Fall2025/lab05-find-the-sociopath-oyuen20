04236afeb08a21509f23afe3b03b7fb85beac32e
Octrina Yuen <oyuen20@gmail.com>
2025-11-24 21:03:41 -0800

    lab05

--- sociopath/app/src/main/java/Sociopath.java
+++ sociopath/app/src/main/java/Sociopath.java
@@ -4,6 +4,28 @@ import java.util.List;
 public class Sociopath {
 
 	public int findTheSociopath (int groupSize, List<int []> likeList) {
-		return 0;
+		if(groupSize <= 0){
+			return -1;
+		}
+		int[] count = new int[groupSize + 1];
+		boolean[] like = new boolean[groupSize + 1];
+		for(int[] person : likeList){
+			int liker = person[0];
+			int liked = person[1];
+			if(liker < 1 || liker > groupSize || liked < 1 || liked > groupSize){
+				continue;
+
+			}
+			like[liker] = true;
+			count[liked]++;
+		}
+
+		for(int person = 1; person <= groupSize; person++){
+			if(!like[person] && count[person] == groupSize - 1){
+				return person;
+			}
+		}
+
+		return -1;
 	}
 }
